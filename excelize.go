68733f99ecfabb63436ec01967bde1bb0e996117
Nguyễn Lương Bằng <Nguyn Lng Bng>
2017-03-22 19:15:11 +0700

    Add SetRowStr

--- excelize.go
+++ excelize.go
@@ -165,6 +165,39 @@ func (f *File) SetCellStr(sheet, axis, value string) {
 	xlsx.SheetData.Row[xAxis].C[yAxis].V = value
 }
 
+//SetRowStr provides function to set string type value of a row
+func (f *File) SetRowStr(sheet string, rowIdx int, value ...*string) {
+	var xlsx xlsxWorksheet
+	name := "xl/worksheets/" + strings.ToLower(sheet) + ".xml"
+	xml.Unmarshal([]byte(f.readXML(name)), &xlsx)
+	if f.checked == nil {
+		f.checked = make(map[string]bool)
+	}
+	ok := f.checked[name]
+	if !ok {
+		checkRow(&xlsx)
+		f.checked[name] = true
+	}
+
+	sz := len(value)
+	for i := 0; i < sz; i++ {
+		xAxis := rowIdx - 1
+		yAxis := i
+
+		rows := xAxis + 1
+		cell := yAxis + 1
+
+		completeRow(&xlsx, rows, cell)
+		completeCol(&xlsx, rows, cell)
+
+		xlsx.SheetData.Row[xAxis].C[yAxis].T = "str"
+		xlsx.SheetData.Row[xAxis].C[yAxis].V = *value[i]
+	}
+
+	output, _ := xml.Marshal(xlsx)
+	f.saveFileList(name, replaceWorkSheetsRelationshipsNameSpace(string(output)))
+}
+
 // SetCellDefault provides function to set string type value of a cell as
 // default format without escaping the cell.
 func (f *File) SetCellDefault(sheet, axis, value string) {
